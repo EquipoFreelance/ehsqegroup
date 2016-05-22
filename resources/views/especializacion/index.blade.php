c4ce7fcf2899cf56c07001b6fae98c054e7143a6
Juan Rodas <juancitowillyr46@gmail.com>
2016-05-21 19:38:33 -0500

    Aplicando Plantillas Blade al metodo index de EspecializacionController

--- resources/views/especializacion/index.blade.php
+++ resources/views/especializacion/index.blade.php
@@ -1,24 +1,58 @@
 @extends('layouts.app_internas')
 
-@section('title', 'Dashboard Secretaria Académica - Especializaciones')
+@section('title', 'Dashboard - Secretaria Académica Especialización')
+
+@section('sidebar_menu')
+@include('dashboard.dashboard_sa_menu')
+@stop
 
 @section('content')
+  <div class="page-title">
+    @if(Session::has('message'))
+        <div class="alert alert-info">
+            {{ Session::get('message') }}
+        </div>
+    @endif
+    <h1>Listado de Especializaciones</h1>
+    <p style="margin-top: 15px">En este interior usted podrá ingresar una nueva Especialización, editar los datos ingresados, realizar la búsqueda de alguna de las Especializaciones, Exportar a Excel, Imprimir y generar un archivo PDF.</p>
+  </div>
+  <div class="clearfix"></div>
+  <div class="row">
+    <!-- INICIO TABLA FINAL -->
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="x_panel">
 
-  @include('dashboard.dash_welcome')
-
-  @include('dashboard.dashboard_sa_menu')
-
-  <a href="{{ route('dashboard.esp.create') }}">Crear</a><br>
-
-  <h1>Listado de Especializaciones</h1><br>
-  @if(Session::has('message'))
-      <div class="alert alert-info">
-          {{ Session::get('message') }}
+        <a href="{{ route('dashboard.esp.create') }}" class="btn btn-success">Agregar</a>
+        <div class="ln_solid"></div>
+        <div class="x_content">
+          <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+            <thead>
+              <tr>
+                <th>Código</th>
+                <th>Especialización</th>
+                <th>Estado</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($esps as $esp)
+                  <tr data-id="{{ $esp->id }}">
+                    <td class="data-cod" data-cod="{{ $esp->id }}">{{ $esp->id }}</td>
+                    <td class="data-name" data-name="{{ $esp->nom_esp_tipo }}">{{ $esp->nom_esp }}</td>
+                    <td class="data-acti" data-acti="{{ $esp->activo }}">
+                      <span class="label @if($esp->activo == '1') label-success @else label-danger @endif ">
+                        @if($esp->activo == '1') Activo @else No Activo @endif
+                      </span>
+                    </td>
+                    <td><a href="{{ route('dashboard.esp.edit', $esp->id) }}" class="btn btn-link">Editar</a></td>
+                  </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
-  @endif
-  <ul>
-    @foreach ($esps as $esp)
-        <li>{{ $esp->nom_esp }} - {{ $esp->id }} <a href="{{ route('dashboard.esp.edit', $esp->id) }}">Editar</a></li>
-    @endforeach
-  </ul>
+    </div>
+    <!-- FINAL TABLA FINAL -->
+  </div>
+
 @stop
