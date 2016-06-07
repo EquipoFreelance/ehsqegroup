4309972d21c96788c408a9aea466512089a1db41
Juan Rodas <juancitowillyr46@gmail.com>
2016-06-07 01:27:06 -0500

    Aplicando relacion de uno a muchos, Grupo y sede

--- app/Grupo.php
+++ app/Grupo.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model
 {
-    //
+      /**
+       * Un Grupo pertenece a una Sede
+      */
+      public function sede()
+      {
+          return $this->belongsTo('App\Sede');
+      }
+
 }
--- app/Sede.php
+++ app/Sede.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sede extends Model
 {
-    //
+    /**
+     * Una sede tiene de uno a muchos Grupos
+    */
+    public function grupos()
+    {
+        return $this->hasMany('App\Models\Grupo', 'cod_sede', 'id');
+    }
+
 }
