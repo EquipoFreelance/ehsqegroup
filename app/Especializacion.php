cd2feb58cc4c5c5a157a445049e226518f21fce1
Juan Rodas <juancitowillyr46@gmail.com>
2016-06-10 00:52:53 -0500

    Modulo Modalidad: Tabla asociada y relaciones con el modelo especializacion

--- app/Especializacion.php
+++ app/Especializacion.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Especializacion extends Model
 {
+    // Nombre de la tabla asociada
     protected $table = 'especializacion';
 
+    // Campos activos
     protected $attributes = array(
        'deleted' => 0,
     );
 
+    // Atributos con valores por defecto
     protected $fillable = [
         'nom_esp', 'activo'
     ];
@@ -24,6 +27,11 @@ class Especializacion extends Model
     // Una especializacion tiene muchos
     public function grupos(){
         return $this->hasMany('App\Models\Grupo', 'cod_esp', 'id');
+    }
+
+    // Una especialicion pertenece a un tipo de modalidad
+    public function modalidad(){
+        return $this->belongsTo('App\Models\Modalidad', 'cod_mod');
     }
 
 }
---
+++ app/Models/Modalidad.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Modalidad extends Model
+{
+    // Nombre de la tabla asociada
+    protected $table = 'modalidad';
+
+    // Campos actvos
+    protected $fillable = [
+        'nom_modalidad',
+        'activo'
+    ];
+
+    // Atributos con valores por defecto
+    protected $attributes = array(
+       'deleted' => 0,
+    );
+
+    //Una Modalidad tiene de uno a muchas Especializaciones
+    public function especializaciones()
+    {
+        return $this->hasMany('App\Especializacion', 'cod_mod', 'id');
+    }
+
+}
